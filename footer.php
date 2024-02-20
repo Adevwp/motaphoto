5c588b5c499546288d7afc9af1c574ee77e98a47
DevWp <aurelie.devwp@gmail.com>
2024-02-20 14:58:01 +0100

    insertion header et footer

--- footer.php
+++ footer.php
@@ -1,3 +1,19 @@
     <!-- FOOTER -->
-    </body>
+    <!-- TODO test footer -->
+
+    <footer>
+    <div class="menu-footer">
+        <nav>
+            <?php /*affiche mon menu footer */
+            wp_nav_menu([
+                'theme_location' => 'footer',
+            ]); 
+            ?>
+            <p> MON FOOTER ICI - Tous droits réservés</p>
+        </nav>
+    </div>
+    </footer>
+
+	<?php wp_footer(); ?>
+</body>
 </html>
--- header.php
+++ header.php
@@ -1,9 +1,22 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title><?php wp_title(); ?></title>
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
     <!-- HEADER -->
-    <!doctype html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Mota Photo</title>
-    </head>
-<body>
+
+    <header>
+        <div>
+            Mon menu ICI
+        </div>
+    </header>
