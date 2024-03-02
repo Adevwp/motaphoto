31c8937d5795882db37fafef9812459f809d9cc6
DevWp <aurelie.devwp@gmail.com>
2024-03-02 20:01:38 +0100

    avancee sur le template de single-photo

--- single-photo.php
+++ single-photo.php
@@ -1,19 +1,72 @@
 <?php get_header(); ?>
 
-<h1 class="site__heading"><?php post_type_archive_title(); ?></h1>
+<?php
+    // Get Taxonomy creat with ACF 
+    $reference = get_field('reference');
+    $type = get_field('type');
+    $year = get_field('annee'); 
+    $photo_id = get_field('photo');
+?>
 
-<main class="site__portfolio">
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    	<div class="project">
-        	<h2 class="project__title">
-                <a href="<?php the_permalink(); ?>">
-                	<?php the_title(); ?>
-                </a>
-            </h2>
-         	<?php the_post_thumbnail(); ?>
-    	</div>
-    <?php endwhile; endif; ?>
-</main> 
 
-<?php the_posts_pagination(); ?>
+<div class="single-photo">
+    <div class="single-photo-info">   <!-- TODO À METTRE À JOUR single-photo-container -->  
+   
+        <div class="single-photo-info_content"> <!-- TODO À METTRE À JOUR single-photo__info-->  
+
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    
+                <h2><?php the_title(); ?></h2>
+
+                <?php if ($reference) {
+                        echo '<p class="description_photo"> Référence : <span id="reference">' . $reference . '</span></p>';
+                } ?>
+
+                <?php
+                    // TODO Récupérer les termes de la taxonomie pour le CPT actuel
+                    $taxoCategorie = get_the_terms(get_the_ID(), 'categorie');
+                    if ($taxoCategorie && !is_wp_error($taxoCategorie)) {
+                        echo '<p class="description_photo">Catégorie : ';
+                        foreach ($taxoCategorie as $taxoCategorie) {
+                            echo '<a href="' . get_term_link($taxoCategorie) . '">' . $taxoCategorie->name . '</a> ';
+                        }
+                        echo '</p>';
+                } ?>
+
+                <?php
+                $taxoFormat = get_the_terms(get_the_ID(), 'format');
+                if ($taxoFormat && !is_wp_error($taxoFormat)) {
+                    echo '<p class="description_photo">Format : ';
+                    foreach ($taxoFormat as $taxoFormat) {
+                        echo '<a href="' . get_term_link($taxoFormat) . '">' . $taxoFormat->name . '</a> ';
+                    }
+                    echo '</p>';
+                } ?>
+
+                <?php if ($type) {
+                    echo '<p class="description_photo"> Type : ' . $type . '</p>';
+                } ?>
+
+                <?php if ($year) {
+                    echo '<p class="description_photo">Année : ' . date('Y', strtotime($year)) . '</p>';
+                } ?>
+
+                <?php endwhile;
+            endif; ?>
+        </div>
+
+        <div class="single-photo-info__image"> <!-- TODO À METTRE À JOUR single-photo__image -->  
+
+            <?php if ($photo_id) { // Vérifie si la photo existe
+            // Si le champ retourne l'ID de l'image, utilise wp_get_attachment_image_url
+            $photo_url = is_array($photo) ? wp_get_attachment_image_url($photo_id['id'], 'full') : $photo_id;
+            echo '<img src="' . esc_url($photo_url) . '" alt="Description" />';
+            } ?>
+       
+    </div>
+    
+    </div>
+</div>
+
+
 <?php get_footer(); ?>
